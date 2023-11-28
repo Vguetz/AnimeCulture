fff1fba5c475df4a2f076821fa319e10c8664e9a
Vguetz <gomezlucas712@gmail.com>
2023-11-27 22:36:47 -0300

    fix:bugfix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,8 +12,7 @@ function Hero() {
           className="object-contain"
         />
         <h1 className="sm:text-6xl text-5xl text-white lg:max-w-lg font-bold leading-[120%]">
-          Explore The <span className="red-gradient">Diverse Realms</span> of
-          Anime Magic
+          Welcome to <span className="red-gradient">a cultural place</span> for Anime Fans
         </h1>
       </div>
       <div className="lg:flex-1 relative w-full h-[50vh] justify-center">
